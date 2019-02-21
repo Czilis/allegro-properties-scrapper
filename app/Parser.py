69d2b4a23025785243a22d9cf227ea32865cd429
Patryk Horodenski <patryk.horodenski@external.immmr.com>
2019-02-21 21:48:28 +0100

    Added filtering elements by class

--- app/Parser.py
+++ app/Parser.py
@@ -1,7 +1,5 @@
 import requests
-from bs4 import BeautifulSoup, Tag
-
-from app import format_table_for_shop
+from bs4 import BeautifulSoup
 
 
 class Parser:
@@ -24,24 +22,16 @@ class Parser:
                     param_name not in self.__EXCLUDED_PARAMETERS}
         return filtered
 
-    def __can_proceed(self, element):
-        return isinstance(element, Tag) and (len(element.contents) > 1)
+    def __map_parameter_table(self, parameter_table: BeautifulSoup):
+        result = parameter_table.find_all("div", class_="_18da3096")
+        for element in result:
+            if len(element.contents) == 2:
+                self.__paired_parameters.update({element.contents[0].string: element.contents[1].string})
 
-    def __map_parameter_table(self, children):
-        for element in children:
-            if self.__can_proceed(element):
-                self.__map_parameter_table(element.contents)
-            elif len(element.contents) != 0:
-                values = element.contents[0].contents
-                if len(values) > 1:
-                    self.__paired_parameters.update({values[0].string: values[1].string})
-                else:
-                    self.__paired_parameters.update(
-                        {values[0].contents[0].contents[0].string: values[0].contents[0].contents[1].string})
         return self.__paired_parameters
 
     def __parse(self, html):
         webpage_entity = BeautifulSoup(html.text)
         parameter_table_parent = webpage_entity.find(attrs={"data-prototype-id": "allegro.showoffer.parameters"})
         parameter_table: BeautifulSoup = parameter_table_parent.contents[1].contents[0]
-        return self.__map_parameter_table(parameter_table.contents)
+        return self.__map_parameter_table(parameter_table)
